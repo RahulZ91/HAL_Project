d5e3fe0652912647fa633482ab63e581118e9fce
Rahul Surange <51949997+RahulZ91@users.noreply.github.com>
2025-12-11 22:32:28 +0100

    Added the callback logic to capture the value of the CCR1 for the first and the second rising edge and to compute the frequency of the input signal

--- Input_Capture_Frequency_Measurement/Core/Inc/main.h
+++ Input_Capture_Frequency_Measurement/Core/Inc/main.h
@@ -31,7 +31,8 @@ extern "C" {
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#define TRUE 1
+#define FALSE 0
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/
