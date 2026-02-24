0d020c2b891bca5287ca2ea3d4febe72133de1b0
Rahul Surange <51949997+RahulZ91@users.noreply.github.com>
2026-02-24 21:51:47 +0100

    Updated the task notifications and added the interrupt for PA8 on stm32f446re nucleo board

--- Orientation and LED Control with MPU6050 and STM32/Core/Inc/main.h
+++ Orientation and LED Control with MPU6050 and STM32/Core/Inc/main.h
@@ -67,6 +67,9 @@ void Error_Handler(void);
 #define USART_RX_GPIO_Port GPIOA
 #define LD2_Pin GPIO_PIN_5
 #define LD2_GPIO_Port GPIOA
+#define MPU60550_INT_Pin GPIO_PIN_8
+#define MPU60550_INT_GPIO_Port GPIOA
+#define MPU60550_INT_EXTI_IRQn EXTI9_5_IRQn
 #define TMS_Pin GPIO_PIN_13
 #define TMS_GPIO_Port GPIOA
 #define TCK_Pin GPIO_PIN_14
